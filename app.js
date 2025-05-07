973561e56044eaf651321eb81d8a48a7efe21390
JamesGames7 <crask26@pickeringcollege.on.ca>
2025-05-07 19:42:16 -0400

    Add service worker

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ function addTask() {
         input.value = ''; // Clear the input
     }
 }
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    var CACHE_NAME = 'ophy-todo-cache-v1';
+    var urlsToCache = [
+        '/',
+        '/style.css',
+        '/app.js'
+    ];
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
